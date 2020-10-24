cd9e94099dc28bf3a0a001b7e86814ecb6a81e32
nachiketj199 <nachiketjoshi61@gmail.com>
2020-10-24 16:16:22 +0530

    Updates

---
+++ precache-manifest.85b9a0981af2750dba4c660a460ebd6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35da60fb810586604faec24ec192e111",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "ddf772752b356bc04eeb",
+    "url": "/todo-react/static/css/main.9c6e52e8.chunk.css"
+  },
+  {
+    "revision": "e656a5ae134656000023",
+    "url": "/todo-react/static/js/2.1d81e151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.1d81e151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf772752b356bc04eeb",
+    "url": "/todo-react/static/js/main.681d51fd.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
