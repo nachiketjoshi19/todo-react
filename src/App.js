5a6167bec10cc47a1f69bdbaa16a689a4722a01f
nachiketj199 <nachiketjoshi61@gmail.com>
2020-10-24 15:38:29 +0530

    todo list with temp storage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  //states
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //useEffect
+  useEffect(() => {
+    filteredHandler();
+  }, [todos, status]);
+
+  const filteredHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'pending':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
   return (
     <div className="App">
       <header>
-        <h1>NJ's To Do list</h1>
+        <h1>NJ's To Do list </h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        todos={todos}
+        inputText={inputText}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,43 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ setInputText, setTodos, todos, inputText, setStatus }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.floor(Math.random() * 100000),
+      },
+    ]);
+    setInputText('');
+    document.querySelector('.todo-input').focus();
+  };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
   return (
     <form>
-      <input type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+        autoFocus
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">all</option>
           <option value="completed">completed</option>
           <option value="pending">pending</option>
